c8c79db1277fa0ad375464c47a0fffc131aab321
yuk7-0v0 <91721784+yuk7-0v0@users.noreply.github.com>
2023-03-16 23:01:13 +0800

    Completion Unit class

--- app/src/main/java/com/yuk7/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/yuk7/coolweather/util/HttpUtil.java
@@ -1,5 +1,13 @@
 package com.yuk7.coolweather.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class HttpUtil {
-    public static void sendOkHttpRequest(String address, okhttp3.)
+    // 发送请求，在回调函数中的onResponse里处理响应数据
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        okHttpClient.newCall(request).enqueue(callback);
+    }
 }
